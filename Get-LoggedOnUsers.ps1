f5c405e840b2f73809d5fd74d9b8a7b9348e2f68
AshishArya_In <56192979+ashisharya65@users.noreply.github.com>
2022-01-19 16:24:40 +0000

    Add files via upload

---
+++ Get-LoggedOnUsers.ps1
@@ -0,0 +1,34 @@
+function Get-LoggedOnUser {
+    [CmdletBinding()]
+    param (
+        [parameter(ValueFromPipeline)]
+        [string[]]$Computername
+        
+    )
+    process{
+
+                foreach ($comp in $ComputerName)
+                {
+                    $processinfo = Get-WmiObject -Query "select * from win32_process where name='explorer.exe'" -ComputerName $comp
+                    $Username = $processinfo | ForEach-Object { $_.GetOwner().User } | Sort-Object -Unique
+    
+                        $myobject = [PSCustomObject]@{
+                        ComputerName = $comp 
+                        UserName = $Username
+                        }                  
+                }        
+                 
+                write-host "===================================================================" -ForegroundColor 'Green'
+                write-host "Number of Users logged into $($myobject.ComputerName) computer are:" -ForegroundColor 'Green'
+                write-host "===================================================================" -ForegroundColor 'Green'
+                $AllUsernames = $($myobject.Username) -join "`n" 
+                write-host "$($AllUsernames)`n" -ForegroundColor 'Blue'
+
+           }
+}
+write-host
+
+# You just need to change enter the path of text file having server names
+$Servers = get-content -path "Path of the text file which stores server names."
+
+$Servers | Get-LoggedOnUser 
