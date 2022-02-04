9c972dcfba6bba54a96adcc40c4ada743affd362
AshishArya_In <56192979+ashisharya65@users.noreply.github.com>
2022-02-04 08:49:23 +0000

    Create ChangingAndroidDeviceNames.ps1

---
+++ Intune_Scripts/ChangingAndroidDeviceNames.ps1
@@ -0,0 +1,44 @@
+################################################################################################################
+<#
+  * Renaming Intune Android Device names with naming convention "Android-SerialNumber".
+  * Here we are filtering all the android devices using a devicelist having devicenames.
+#>
+################################################################################################################
+
+#Getting All Android phones
+$Devices = Get-IntuneManagedDevice | Get-MSGraphAllPages | Where-Object {($_.operatingSystem -eq "Android")}
+
+#Getting the device list
+$DeviceList = get-content "C:\Users\Intune.Test\OneDrive - Alzheimer's Society\Desktop\Devicename.txt"
+
+#Filtering the devices with all the properties
+$FilteredDevices = $Devices | where-object -FilterScript { $DeviceList -contains $_.devicename } | Sort-Object -Property devicename
+
+#Traversing all devices and then changing their names as per the defined naming convention
+foreach ($Device in $FilteredDevices) {
+    $OldDeviceName = $Device.deviceName
+    $DeviceID = $Device.Id
+    $NewDeviceName = "Android-" + $Device.serialNumber
+    $Resource = "deviceManagement/managedDevices('$DeviceID')/setDeviceName"
+    $GraphApiVersion = "Beta"
+    $URI = "https://graph.microsoft.com/$graphApiVersion/$($resource)"
+
+    $JSONPayload = @"
+{
+deviceName:"$NewDeviceName"
+}
+"@
+
+    Invoke-MSGraphRequest -HttpMethod POST -Url $URI -Content $JSONPayload -Verbose
+    
+    $Myobject = [PSCustomObject]@{
+        OldDeviceName = $($OldDeviceName)
+        NewDeviceName = $($NewDeviceName)
+        SerialNumber  = $($Device).SerialNumber       
+    }
+
+    [Array] $FinalDevices += $MyObject
+}
+
+#Displaying the object with its defined properties
+$FinalDevices
