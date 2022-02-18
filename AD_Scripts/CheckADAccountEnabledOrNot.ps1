0f8858480d662b61d592c03a3a9770ff9424c667
AshishArya_In <56192979+ashisharya65@users.noreply.github.com>
2022-02-18 13:48:01 +0000

    Create CheckADAccountEnabledOrNot.ps1

---
+++ AD_Scripts/CheckADAccountEnabledOrNot.ps1
@@ -0,0 +1,21 @@
+<#
+    Check whether AD users are enabled or not.
+    
+    Here we have used the userlist text file which stores the user's user principal name (UPN).
+    
+    Author: Ashish Arya
+    Github: @AshishArya_In
+#>
+
+$users = get-content "C:\Users\Ashish.Arya\Desktop\userlist.txt"
+
+$users | foreach-object {
+
+    [PSCustomObject]@{
+
+        UPN = $_
+        Enabled = (Get-ADUser -identity $_.split("@")[0]).Enabled
+
+    }
+
+} | Sort UPN, Enabled
