ae9e53135a97fce0a4a030547594bb3944c141ac
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2022-07-05 19:01:39 +0100

    Update DeletedItemsEmail-Outlook.ps1

--- Misc_Scripts/DeletedItemsEmail-Outlook.ps1
+++ Misc_Scripts/DeletedItemsEmail-Outlook.ps1
@@ -1,3 +1,12 @@
+<#
+    .SYNOPSIS
+    Delete the emails from the deleted items of your Inbox in Outlook client app.
+   
+   .NOTES
+   Author : Ashish Arya (ashisharya65@outlook.com)
+   Date   :  5-July-2022
+#>
+
 Add-Type -Assembly "$($env:ProgramFiles)\Microsoft Office\root\Office16\ADDINS\Microsoft Power Query for Excel Integrated\bin\Microsoft.Office.Interop.Outlook.dll"
 
 $outlookApp = New-Object -comobject Outlook.Application
