9612b6cafcc6765377751dd236e6681b11769fd4
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2023-01-19 22:12:24 +0530

    Update and rename AuthViaMSGraphModule.ps1 to AuthViaMSGraphModule1.ps1

--- Intune_Scripts/Authentication/AuthViaMSGraphModule1.ps1
+++ Intune_Scripts/Authentication/AuthViaMSGraphModule1.ps1
@@ -3,16 +3,15 @@
 $latest = Find-Module -Name Microsoft.Graph -AllVersions -AllowPrerelease | select-Object -First 1
 $current = Get-InstalledModule | Where-Object {$_.Name -eq "Microsoft.Graph"}
 If ($latest.version -gt $current.version) {
-
-    Try {
-          Update-Module -Name Microsoft.Graph -RequiredVersion $latest.version -AllowPrerelease
-          Write-host "Microsoft Graph PowerShell module updated successfully to" $latest.Version -ForegroundColor Green
-    }
-    Catch {
-         Write-Host "Unable to update Microsoft Graph PowerShell module" -ForegroundColor Red
-    }
+        Try {
+                  Update-Module -Name Microsoft.Graph -RequiredVersion $latest.version -AllowPrerelease
+                  Write-Host "Microsoft Graph PowerShell module updated successfully to" $latest.Version -ForegroundColor Green
+        }
+        Catch {
+                  Write-Host "Unable to update Microsoft Graph PowerShell module" -ForegroundColor Red
+        }
 } Else {
-    write-host "Latest version of Microsoft Graph is not newer than the current" -ForegroundColor yellow
+     Write-Host "Latest version of Microsoft Graph is not newer than the current" -ForegroundColor yellow
 }
 
 # Azure AD App details
