56693b868eb6ee620c6c4093bdd1d03f612038fd
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2023-03-17 17:46:53 +0530

    Update GetGroupInfoAssociatedWithUsers.ps1

--- Microsoft-Graph/AzureAD-Group/GetGroupInfoAssociatedWithUsers/GetGroupInfoAssociatedWithUsers.ps1
+++ Microsoft-Graph/AzureAD-Group/GetGroupInfoAssociatedWithUsers/GetGroupInfoAssociatedWithUsers.ps1
@@ -4,10 +4,9 @@
     Getting the Azure AD groups associated with users.
     
     .DESCRIPTION
-    PowerShell script to add a user account as an owner to single AAD group.
+    PowerShell script to get all the Azure AD groups associated with the users mentioned in the csv file (attached to the GitHub repo).
     This script will prompt you to provide a regex pattern for group display names.
-    Here we will be requiring a csv file as well.
-    
+
     .NOTES
     Author : Ashish Arya
     Date   : 13-March-2023
@@ -17,9 +16,9 @@
 $RegExPattern = Read-Host -prompt "Enter your RegEx pattern for Azure AD Group name"
 
 # Declare Azure AD app details
-$ApplicationId = $Env:Azure_CLIENT_ID
-$TenantID = $Env:Azure_TENANT_ID
-$ClientSecret = $Env:Azure_CLIENT_SECRET | ConvertTo-SecureString -AsPlainText -Force
+$ApplicationId = [System.Environment]::GetEnvironmentVariable("Azure_CLIENT_ID")
+$TenantID = [System.Environment]::GetEnvironmentVariable("Azure_TENANT_ID")
+$ClientSecret = [System.Environment]::GetEnvironmentVariable("Azure_CLIENT_SECRET") | ConvertTo-SecureString -AsPlainText -Force
 $ClientSecretCredential = New-Object -TypeName 'System.Management.Automation.PSCredential' -ArgumentList $ApplicationId, $ClientSecret
 
 # Connecting to Microsoft Graph
