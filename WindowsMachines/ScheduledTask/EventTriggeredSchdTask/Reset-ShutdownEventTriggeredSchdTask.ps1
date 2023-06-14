d74f848469764b8fd6f0284c1c643ef9a5c1ddee
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2023-06-14 23:37:00 +0530

    Update and rename Reset-AVDShutdownEventTriggeredSchdTask.ps1 to Reset-ShutdownEventTriggeredSchdTask.ps1

--- WindowsMachines/ScheduledTask/EventTriggeredSchdTask/Reset-ShutdownEventTriggeredSchdTask.ps1
+++ WindowsMachines/ScheduledTask/EventTriggeredSchdTask/Reset-ShutdownEventTriggeredSchdTask.ps1
@@ -1,10 +1,10 @@
 <#
   .SYNOPSIS
-    PowerShell Script to Disable & Enable the AVD shutdown event triggered schd task after the user connect event is triggered.
+    PowerShell script to Disable & Enable the device shutdown event triggered schd task after the user connect is triggered.
 
   .DESCRIPTION
-    With this PowerShell script, an event triggered scheduled task will be created on task scheduler of every AVD session host which will reset disable & enable
-    the AVD shutdown event triggered schedule task whenever the user 'connect' event is triggered
+    With this PowerShell script, an event triggered schedule task will be created on the task scheduler of windows device which
+    will disable & then enable the device shutdown event triggered schedule task whenever the user connect event is triggered.
 
   .NOTES
     Author : Ashish Arya
@@ -14,14 +14,19 @@
     .\Reset-AVDShutdownEventTriggeredSchdTask.ps1
 #>
 
+Param(
+    [parameter(mandatory)]
+    $$PathOfResetScript
+)
+
 $TaskName = "Reset AVD Shutdown Event Triggered Schd task"
 $Taskdescription = "Disable & Enable AVD shutdown event triggered schd task when User Connect event is triggered."
-$TaskAction = New-ScheduledTaskAction -Execute "C:\Users\Hitakshi\Downloads\Reset.ps1"
+$TaskAction = New-ScheduledTaskAction -Execute $PathOfResetScript
 $CIMTriggerClass = Get-CimClass -ClassName 'MSFT_TaskSessionStateChangeTrigger' -Namespace 'Root/Microsoft/Windows/TaskScheduler:MSFT_TaskEventTrigger'
 $TaskTrigger = New-CimInstance -CimClass $CIMTriggerClass -ClientOnly
-$TaskTrigger.StateChange = "RemoteConnect"
+$TaskTrigger.StateChange = "3"
 $TaskTrigger.Enabled = $True
 $TaskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -Compatibility 'Win8'
 $TaskPrincipal = New-ScheduledTaskPrincipal -UserId 'SYSTEM' -LogonType 'ServiceAccount' -RunLevel 'Highest'
 $Task = New-ScheduledTask -Description $Taskdescription -Action $TaskAction -Settings $TaskSettings -Trigger $TaskTrigger -Principal $TaskPrincipal
-Register-ScheduledTask -InputObject $Task -TaskName $TaskName -ErrorVariable $RegSchedTaskError
+Register-ScheduledTask -InputObject $Task -TaskName $TaskName 
