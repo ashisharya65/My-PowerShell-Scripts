ded13567b8707fa9fb754f10bb278abf2efe59f8
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2023-06-15 00:07:05 +0530

    Update Reset-ShutdownEventTriggeredSchdTask.ps1

--- WindowsMachines/ScheduledTask/EventTriggeredSchdTask/Reset-ShutdownEventTriggeredSchdTask.ps1
+++ WindowsMachines/ScheduledTask/EventTriggeredSchdTask/Reset-ShutdownEventTriggeredSchdTask.ps1
@@ -3,8 +3,8 @@
     PowerShell script to Disable & Enable the device shutdown event triggered schd task after the user connect is triggered.
 
   .DESCRIPTION
-    With this PowerShell script, an event triggered schedule task will be created on the task scheduler of windows device which
-    will disable & then enable the device shutdown event triggered schedule task whenever the user connect event is triggered.
+    With this PowerShell script, an event triggered schedule task will be created on windows device and will be triggered to disable & then 
+    enable the device shutdown event triggered schedule task, whenever the user connect event is generated in the event viewer.
 
   .NOTES
     Author : Ashish Arya
@@ -16,17 +16,17 @@
 
 # Prompt to get the path of reset script. You will find the reset script
 Param(
-    [Parameter(Mandatory)]
-    $PathOfResetScript
+  [Parameter(Mandatory)]
+  $PathOfResetScript
 )
 
 # Mentioning Scheduled task details like name, description, trigger & action.
-$TaskName = "Reset Shutdown Event Triggered Schd task"
-$Taskdescription = "Disable & Enable device shutdown event triggered schd task when User Connect event is triggered."
+$TaskName = "Reset AVD Shutdown Event Triggered Schd task"
+$Taskdescription = "Disable & Enable AVD shutdown event triggered schd task when User Connect event is triggered."
 $TaskAction = New-ScheduledTaskAction -Execute $PathOfResetScript
 $CIMTriggerClass = Get-CimClass -ClassName 'MSFT_TaskSessionStateChangeTrigger' -Namespace 'Root/Microsoft/Windows/TaskScheduler:MSFT_TaskEventTrigger'
 $TaskTrigger = New-CimInstance -CimClass $CIMTriggerClass -ClientOnly
-$TaskTrigger.StateChange = "3"     # This will enable the "Connection from Remote computer" radio button in trigger settings of schd task
+$TaskTrigger.StateChange = "3"     # This will enable the "Connection from Remote computer" radio button in settings of schd task
 $TaskTrigger.Enabled = $True
 $TaskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -Compatibility 'Win8'
 $TaskPrincipal = New-ScheduledTaskPrincipal -UserId 'SYSTEM' -LogonType 'ServiceAccount' -RunLevel 'Highest'
