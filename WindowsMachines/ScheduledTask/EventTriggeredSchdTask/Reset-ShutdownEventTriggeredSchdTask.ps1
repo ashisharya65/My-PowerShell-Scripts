5100848c2f5859c6094ca95c978aa843d3a32370
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2023-06-14 23:57:00 +0530

    Update Reset-ShutdownEventTriggeredSchdTask.ps1

--- WindowsMachines/ScheduledTask/EventTriggeredSchdTask/Reset-ShutdownEventTriggeredSchdTask.ps1
+++ WindowsMachines/ScheduledTask/EventTriggeredSchdTask/Reset-ShutdownEventTriggeredSchdTask.ps1
@@ -21,12 +21,12 @@ Param(
 )
 
 # Mentioning Scheduled task details like name, description, trigger & action.
-$TaskName = "Reset AVD Shutdown Event Triggered Schd task"
-$Taskdescription = "Disable & Enable AVD shutdown event triggered schd task when User Connect event is triggered."
+$TaskName = "Reset Shutdown Event Triggered Schd task"
+$Taskdescription = "Disable & Enable device shutdown event triggered schd task when User Connect event is triggered."
 $TaskAction = New-ScheduledTaskAction -Execute $PathOfResetScript
 $CIMTriggerClass = Get-CimClass -ClassName 'MSFT_TaskSessionStateChangeTrigger' -Namespace 'Root/Microsoft/Windows/TaskScheduler:MSFT_TaskEventTrigger'
 $TaskTrigger = New-CimInstance -CimClass $CIMTriggerClass -ClientOnly
-$TaskTrigger.StateChange = "3"     # This will enable the "Connection from Remote computer" radio button in settings of schd task
+$TaskTrigger.StateChange = "3"     # This will enable the "Connection from Remote computer" radio button in trigger settings of schd task
 $TaskTrigger.Enabled = $True
 $TaskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -Compatibility 'Win8'
 $TaskPrincipal = New-ScheduledTaskPrincipal -UserId 'SYSTEM' -LogonType 'ServiceAccount' -RunLevel 'Highest'
