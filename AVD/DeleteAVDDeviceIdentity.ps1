8508a3e4ee3e1789349afd4568e032e2652bd1f9
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2023-09-07 20:05:37 +0530

    Update DeleteAVDDeviceIdentity.ps1

--- AVD/DeleteAVDDeviceIdentity.ps1
+++ AVD/DeleteAVDDeviceIdentity.ps1
@@ -29,6 +29,7 @@ Param(
 $Devices = Get-Content -path $TextFilePath 
 
 # Checking and Verifying if the latest version of Az, Microsoft.Graph & Az.DestopVirtualization powerShell modules are installed or not
+Write-Host "Checking the required PowerShell modules are already installed or not..." -ForegroundColor 'Yellow'
 $Modules = @(
     "Az",
     "Microsoft.Graph",
@@ -39,6 +40,7 @@ Foreach ($Module in $Modules) {
     $current = Get-InstalledModule | Where-Object { $_.Name -eq $Module }
     If ($latest.version -gt $current.version) {
         Try {
+            Write-Host "Latest version $($latest.version) of $($Module) 7is not installed. Hence installing it..." -ForegroundColor 'Yellow'
             Update-Module -Name $Module -RequiredVersion $latest.version -AllowPrerelease -Force -ErrorAction 'Stop'
             Write-Host "$($Module) PowerShell module has been successfully updated to $($latest.Version) version."-ForegroundColor 'Green'
         }
@@ -50,7 +52,7 @@ Foreach ($Module in $Modules) {
         Try {
             Write-Host "The $($Module) PowerShell module is not installed. Hence, installing it..." -ForegroundColor "Yellow"
             Install-Module $Module -scope CurrentUser -force -ErrorAction 'Stop'
-            Write-Host "The $($Module) PowerShell module with $($latest.version) version has been installed successfully on your machine."
+            Write-Host "The $($Module) PowerShell module with $($latest.version) version has been installed successfully on your machine." -ForegroundColor 'Green'
         }
         Catch {
             Write-Host "Unable to install the $($Module) PowerShell module with $($latest.version) version." -ForegroundColor 'Red'
@@ -62,12 +64,16 @@ Foreach ($Module in $Modules) {
 }
 
 # Connecting to Azure
+Write-Host "`nConnecting to Azure...." -ForegroundColor 'Yellow'
 Connect-AzAccount -Tenant $TenantID -Subscription $Subscription  | Out-Null
+Write-Host "Connected to Azure." -ForegroundColor 'Green'
 
 # Connecting to Microsoft Graph
+Write-Host "`nConnecting to Microsoft Graph API...." -ForegroundColor 'Yellow'
 $AADRegisteredAppSecret = $AADRegisteredAppSecret | ConvertTo-SecureString -AsPlainText -Force
 $ClientSecretCredential = New-Object -TypeName 'System.Management.Automation.PSCredential' -ArgumentList $AzureADRegisteredAppId, $AADRegisteredAppSecret
 Connect-MgGraph -TenantId $TenantID -ClientSecretCredential $ClientSecretCredential | Out-Null
+Write-Host "Connected To Microsoft Graph API." -ForegroundColor 'Green'
 
 # Function to remove the AVD from the Azure VM portal
 Function Remove-AVDFromAzureVMPortal {
@@ -79,7 +85,7 @@ Function Remove-AVDFromAzureVMPortal {
     $DeviceIdentity.StorageProfile.OsDisk.DeleteOption = 'Delete'
     $DeviceIdentity.StorageProfile.DataDisks | ForEach-Object { $_.DeleteOption = 'Delete' }
     $DeviceIdentity.NetworkProfile.NetworkInterfaces | ForEach-Object { $_.DeleteOption = 'Delete' }
-    $DeviceIdentity | Update-AzVM | Out-Null
+    $DeviceIdentity | Update-AzVM -NoWait | Out-Null
 
     Try {
         Remove-AzVm -ResourceGroupName $AVDResourceGroup -Name $DeviceIdentity.Name -ForceDeletion $true -Force -NoWait | Out-Null
@@ -96,7 +102,7 @@ $Count = $null
 Foreach ($Device in $Devices) {
 
     $Count += 1
-    Write-Host "`n$($Count). For $($Device) device" -ForegroundColor 'DarkCyan'
+    Write-Host "`n$($Count). For $($Device) AVD" -ForegroundColor 'Cyan'
     Write-Host "****************************************************************************************************" -ForegroundColor 'Cyan'
 
     # Shutting down the AVD if it is in running state
@@ -105,12 +111,15 @@ Foreach ($Device in $Devices) {
         Write-Host "The $($Device) AVD is still running. Hence shutting it down now." -ForegroundColor "Yellow"
         Try {
             Stop-AzVM -ResourceGroupName $AVDResourceGroup -Name $Device -ErrorAction 'Stop' -Force -NoWait
-            Write-Host "$($Device) has been sucessfully stopped." -ForegroundColor 'Green'
+            Write-Host "$($Device) AVD has been sucessfully stopped." -ForegroundColor 'Green'
         }
         Catch {
             $errormessage = "Unable to shutdown the $($Device) AVD."
             Write-Error $errormessage
         }
+    }
+    Elseif ($null -eq $AVDPowerState) {
+        Write-Host "$($Device) AVD is not found on Azure VM portal." -ForegroundColor 'Yellow'
     }
 
     # Removing the device identiy from AVD hostpool portal
@@ -119,6 +128,7 @@ Foreach ($Device in $Devices) {
         Write-Host "$($Device) AVD does not exist in the AVD portal." -ForegroundColor 'Yellow'
     }
     Else {
+        Write-Host "$($Device) AVD is existing in AVD hostpool portal. Hence deleting it..." -ForegroundColor 'Yellow'
         Try {
             Remove-AzWVDSessionHost -ResourceGroupName $HPResourceGroup -HostpoolName $HostPoolName -Name $Device -ErrorAction 'Stop'
             Write-Host "The device identity of $($Device) AVD has been successfully deleted from the AVD host pool portal." -ForegroundColor 'Green'
@@ -135,6 +145,7 @@ Foreach ($Device in $Devices) {
         Write-Host "$($Device) AVD does not exist in the Azure VM portal." -ForegroundColor 'Yellow'
     }
     Else {
+        Write-Host "$($Device) AVD is existing in Azure VM portal. Hence deleting it..." -ForegroundColor 'Yellow'
         Remove-AVDFromAzureVMPortal -DeviceIdentity $AzureVMPortalIdentity
     }
 
@@ -144,12 +155,13 @@ Foreach ($Device in $Devices) {
         Write-Host "$($Device) AVD does not exist in the Intune portal." -ForegroundColor 'Yellow'
     }
     Else {
+        Write-Host "$($Device) AVD is existing in Intune portal. Hence deleting it..." -ForegroundColor 'Yellow'
         Try {
             Remove-MgDeviceManagementManagedDevice -ManagedDeviceId $IntuneDeviceIdentity.Id
             Write-Host "The device identity of $($Device) AVD device has been successfully deleted from the Intune portal." -ForegroundColor 'Green'
         }
         Catch {
-            $errormessage = "Unable to delete the Intune device identity."
+            $errormessage = "Unable to delete the $($Device) AVD from Intune portal."
             Write-Error $errormessage
         }
     }
@@ -160,12 +172,13 @@ Foreach ($Device in $Devices) {
         Write-Host "$($Device) AVD does not exist in the Intune portal." -ForegroundColor 'Yellow'
     }
     Else {
+        Write-Host "$($Device) AVD is existing in Azure AD portal. Hence deleting it..." -ForegroundColor 'Yellow'
         Try {
             Remove-MgDevice -DeviceId $AzureADDeviceIdentity.id  
             Write-Host "The device identity of $($Device) AVD has been successfully deleted from the Azure AD portal." -ForegroundColor 'Green'
         }
         Catch {
-            $errormessage = "Unable to delete the device identity from Azure AD portal."
+            $errormessage = "Unable to delete the $($Device) AVD from Azure AD portal."
             Write-Error $errormessage
         }
     }
