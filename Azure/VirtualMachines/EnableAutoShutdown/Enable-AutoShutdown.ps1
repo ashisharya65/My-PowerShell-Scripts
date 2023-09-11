d49c52d1084710654b5b325eb7bd15166e48febd
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2023-09-11 23:54:43 +0530

    Create Enable-AutoShutdown.ps1

---
+++ Azure/VirtualMachines/EnableAutoShutdown/Enable-AutoShutdown.ps1
@@ -0,0 +1,67 @@
+
+Connect-AzAccount
+    
+Function Enable-AzVMAutoShutdown {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,
+            HelpMessage = "The Azure subscription id.")] 
+        $subscriptionId,
+        [Parameter(Mandatory,
+            HelpMessage = "Resource group name which contains the Azure VM.")]
+        $resourceGroupName,
+        [Parameter(Mandatory,
+            HelpMessage = "Name of the Azure VM.")] 
+        $vmName,
+        [Parameter(Mandatory,
+            HelpMessage = "Location of Azure VM.")] 
+        $location,
+        [Parameter(Mandatory,
+            HelpMessage = "Set the autoshutdown time in a similar way like this 2215 for 10:15 PM.")] 
+        $time,
+        [Parameter(Mandatory,
+            HelpMessage = "The concerned timezone which you need to set. For eg. Indian Standard Time.")] 
+        $timezone 
+    )
+
+    $token = Get-AzAccessToken
+
+    $authHeader = @{
+        'Content-Type'  = 'application/json'
+        'Authorization' = 'Bearer ' + $token.Token
+    }
+
+    $resource = "$($subscriptionId)/resourceGroups/$($resourceGroupName)/providers/Microsoft.DevTestLab/schedules/shutdown-computevm-$($vmName)?api-version=2018-10-15-preview"
+    $uri = "https://management.azure.com/subscriptions/$($resource)"
+
+    $requestbody = @"
+{
+    "location" : "$($location)",
+    properties : {
+        "dailyRecurrence" : {
+            "time" : "$($time)"
+        },
+        "status" : "Enabled",
+        "targetResourceId" : "/subscriptions/$($subscriptionId)/resourceGroups/$($resourceGroupName)/providers/Microsoft.Compute/virtualMachines/$($vmName)",
+        "taskType" : "ComputeVmShutdownTask",
+        "timeZoneId" : "$($timezone)"
+    }
+}
+"@
+    Try {
+        Write-Host "Enabling the autoshutdown setting on $($vmName) VM" -ForegroundColor 'Yellow' -NoNewline
+        For ($i = 0; $i -le 4; $i++) {
+            Start-sleep 1
+            Write-Host "." -NoNewline -ForegroundColor 'Yellow'     
+        }
+        
+        Invoke-RestMethod -Uri $uri -Method 'PUT' -Headers $authHeader -Body $requestbody -erroraction 'Stop' | Out-Null
+        Write-Host "`nThe autoshutdown setting has been successfully enabled on the $($vmName) VM." -ForegroundColor 'Green'
+    }
+    Catch {
+        $errormessage = $_.Exception.Message
+        Write-Error $errormessage
+    }
+}
+
+
