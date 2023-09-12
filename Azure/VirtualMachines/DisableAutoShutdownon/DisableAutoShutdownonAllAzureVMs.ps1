d108ac665904023808dd8d1cf315e702d2e65630
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2023-09-12 14:02:31 +0530

    Update DisableAutoShutdownonAllAzureVMs.ps1

--- Azure/VirtualMachines/DisableAutoShutdownon/DisableAutoShutdownonAllAzureVMs.ps1
+++ Azure/VirtualMachines/DisableAutoShutdownon/DisableAutoShutdownonAllAzureVMs.ps1
@@ -1,12 +1,12 @@
 
 param(
-    [Parameter(Mandatory, HelpMessage = "The Azure subscription id.")] $tenantId,
-    [Parameter(Mandatory, HelpMessage = "The Azure subscription id.")] $subscription,
-    [Parameter(Mandatory, HelpMessage = "The Azure subscription id.")] $subscriptionId",
-    [Parameter(Mandatory, HelpMessage = "Resource group name which contains the Azure VM.")] $resourceGroupName,
-    [Parameter(Mandatory, HelpMessage = "Location of Azure VM.")] $location,
-    [Parameter(Mandatory, HelpMessage = "Set the autoshutdown time in a similar way like this 2215 for 10:15 PM.")] $time,
-    [Parameter(Mandatory, HelpMessage = "The concerned timezone which you need to set. For eg. Indian Standard Time.")] $timezone
+        [Parameter(Mandatory,HelpMessage = "Enter the Tenand Id.")] $TenantId,
+        [Parameter(Mandatory, HelpMessage = "Enter the name of Azure subscription.")] $subscription,
+        [Parameter(Mandatory, HelpMessage = "The Azure subscription id.")] $subscriptionId,
+        [Parameter(Mandatory, HelpMessage = "Resource group name which contains the Azure VM.")] $resourceGroupName,
+        [Parameter(Mandatory, HelpMessage = "Location of Azure VM.")] $location,
+        [Parameter(Mandatory, HelpMessage = "Set the autoshutdown time in a similar way like this 2215 for 10:15 PM.")] $time,
+        [Parameter(Mandatory, HelpMessage = "The concerned timezone which you need to set. For eg. India Standard Time.")] $timezone 
 )
 
 Function Disable-AzVMAutoShutdown {
