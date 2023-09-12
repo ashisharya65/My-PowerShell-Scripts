b8f98c2ed80f6ef1de24f9eb106e715e143e7de9
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2023-09-12 13:41:15 +0530

    Update Enable-AutoShutdown.ps1

--- Azure/VirtualMachines/EnableAutoShutdown/Enable-AutoShutdown.ps1
+++ Azure/VirtualMachines/EnableAutoShutdown/Enable-AutoShutdown.ps1
@@ -1,27 +1,19 @@
 
-Connect-AzAccount
+param (
+        [Parameter(Mandatory,HelpMessage = "Enter the Tenand Id.")] $TenantId,
+        [Parameter(Mandatory, HelpMessage = "Enter the name of Azure subscription.")] $subscription,
+        [Parameter(Mandatory, HelpMessage = "The Azure subscription id.")] $subscriptionId,
+        [Parameter(Mandatory, HelpMessage = "Resource group name which contains the Azure VM.")] $resourceGroupName,
+        [Parameter(Mandatory, HelpMessage = "Name of the Azure VM.")] $vmName,
+        [Parameter(Mandatory, HelpMessage = "Location of Azure VM.")] $location,
+        [Parameter(Mandatory, HelpMessage = "Set the autoshutdown time in a similar way like this 2215 for 10:15 PM.")] $time,
+        [Parameter(Mandatory, HelpMessage = "The concerned timezone which you need to set. For eg. India Standard Time.")] $timezone 
+)
     
 Function Enable-AzVMAutoShutdown {
-    [CmdletBinding()]
+
     param(
-        [Parameter(Mandatory,
-            HelpMessage = "The Azure subscription id.")] 
-        $subscriptionId,
-        [Parameter(Mandatory,
-            HelpMessage = "Resource group name which contains the Azure VM.")]
-        $resourceGroupName,
-        [Parameter(Mandatory,
-            HelpMessage = "Name of the Azure VM.")] 
-        $vmName,
-        [Parameter(Mandatory,
-            HelpMessage = "Location of Azure VM.")] 
-        $location,
-        [Parameter(Mandatory,
-            HelpMessage = "Set the autoshutdown time in a similar way like this 2215 for 10:15 PM.")] 
-        $time,
-        [Parameter(Mandatory,
-            HelpMessage = "The concerned timezone which you need to set. For eg. India Standard Time.")] 
-        $timezone 
+        $VMName
     )
 
     $token = Get-AzAccessToken
@@ -64,4 +56,5 @@ Function Enable-AzVMAutoShutdown {
     }
 }
 
-
+Connect-AzAccount -Tenant $TenantId -Subscription $Subscription  | Out-Null
+Enable-AzVMAutoShutdown -VMName $vmName
