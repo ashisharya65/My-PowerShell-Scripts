f9590c4c9394888f20335c80c571f0160dfe5f68
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2023-12-06 16:21:59 +0000

    Update Create-DeviceShutdownEventTriggeredSchdTask.ps1

--- WindowsMachines/EventTriggeredSchdTask/Create-DeviceShutdownEventTriggeredSchdTask.ps1
+++ WindowsMachines/EventTriggeredSchdTask/Create-DeviceShutdownEventTriggeredSchdTask.ps1
@@ -37,3 +37,4 @@ $TaskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopI
 $TaskPrincipal = New-ScheduledTaskPrincipal -UserId 'SYSTEM' -LogonType 'ServiceAccount' -RunLevel 'Highest'
 $Task = New-ScheduledTask -Description $Taskdescription -Action $TaskAction -Settings $TaskSettings -Trigger $TaskTrigger -Principal $TaskPrincipal
 Register-ScheduledTask -InputObject $Task -TaskName $TaskName | Out-Null
+
