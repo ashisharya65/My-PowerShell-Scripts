07b91891afb6a405d2a07b6e691c3027850d0fcc
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2024-01-19 14:52:08 +0530

    Update ExtractIntuneAppsWithAssignedGroupsToCSVFile(RestAPI).ps1

--- Intune_Scripts/Apps/GetIntuneAppsWithAssignedGroups/ExtractIntuneAppsWithAssignedGroupsToCSVFile(RestAPI).ps1
+++ Intune_Scripts/Apps/GetIntuneAppsWithAssignedGroups/ExtractIntuneAppsWithAssignedGroupsToCSVFile(RestAPI).ps1
@@ -40,7 +40,7 @@ Function Set-EnvtVariables {
     $EnvtVariables = @(
         [PSCustomObject]@{
             Name  = "AZURE_CLIENT_ID"
-            Value = $CliendId
+            Value = $ClientId
         },
         [PSCustomObject]@{
             Name  = "AZURE_CLIENT_SECRET"
