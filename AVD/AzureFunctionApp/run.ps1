f4a5e0cc6e435df4b0a2c5421a3327c2d3904da5
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2024-02-15 19:23:47 +0100

    Create run.ps1

---
+++ AVD/AzureFunctionApp/run.ps1
@@ -0,0 +1,79 @@
+
+# For the Function App
+param($Timer)
+
+# Recording the starting time for the script execution
+$ScriptStartTime = Get-Date -Format "dd-MM-yyyy hh:mm:ss"
+Write-Output "`n##########################################"
+Write-Output "Script start time: $ScriptStartTime"
+Write-Output "##########################################`n"
+
+###########Variables ############
+$AVDRG = "AVDs resource group name"
+$RegExPattern = "Regular expression pattern for fetching the running AVDs from the above mentioned resource group"
+
+# Declaring the concerned hostpools
+$allHostPools = @(
+    [pscustomobject]@{
+        HostPool   = "HostPool Name"
+        HostPoolRG = "HostPool ResourceGroup"
+    },
+    [PSCustomObject]@{
+        HostPool   = "HostPool Name"
+        HostPoolRG = "HostPool ResourceGroup"
+    }
+)
+
+# Looping through the hostpools
+For ($i = 0; $i -lt $allHostPools.Length; $i++) {
+
+    $pool = $allHostPools[$i].HostPool
+    $poolrg = $allHostPools[$i].HostPoolRG
+
+    Write-Output "########################################################"
+    Write-Output "HostPool Name           : $pool"
+    Write-output "HostPool Resource group : $poolRg"
+    Write-Output "########################################################`n"
+
+    # Getting all the running AVDs which are having the names matching with the RegExPattern
+    $VMs = Get-AzVM -ResourceGroupName $AVDRG -Status | Where-Object { ($_.Name -match "$RegExPattern") -and ($_.PowerState -ne "VM deallocated") } |`
+        Sort-Object { [int]($_.Name -replace '.*-(\d+)', '$1') }
+
+    $Count = 0
+    $DeallocatedVMsCount = 0    
+    # Looping through all the running AVDs
+    Foreach ($VM in $VMs) {
+        $AVDName = $VM.Name          
+        # Getting the AVD details
+        $AVD = Get-AzWvdSessionHost -HostPoolName $Pool -ResourceGroupName $PoolRg -Name "$($AVDName)" -ErrorAction 'Silentlycontinue'        
+        # Selecting those active Win11 AVDs which are there at the AVD portal
+        If(-not(([string]::IsNullOrWhiteSpace($AVD))-or([string]::IsNullOrEmpty($AVD)))){
+            $Count += 1
+            # Stopping those AVDs which are in Drain mode off state and does not have any active session
+            If (($AVD.AllowNewSession -eq $true) -and ($AVD.Session -eq 0)) {
+                Try {
+                    Stop-AzVM -Name $AVDName -ResourceGroupName $VMRG -Force -NoWait -ErrorAction 'Stop' | Out-Null
+                    Write-Output "$count. The $AVDName AVD does not have any active user session. Hence, shutting it down."
+                    $DeallocatedVMsCount += 1
+                }
+                Catch {
+                    $ErrorMessage = $_.Exception.Message
+                    Write-Error ("Error Message: " + $ErrorMessage)
+                }
+            }
+            Else {
+                Write-Output "$Count. The $AVDName AVD already has an active user session."
+            }
+        }
+    }      
+    Write-Output "`n##############################################################################################"               
+    Write-Output "The number of AVDs which got deallocated from the $pool hostpool : $DeallocatedVMsCount"
+    Write-Output "##############################################################################################`n"               
+}
+
+# Recording the end time of script execution
+$ScriptEndTime = Get-Date -Format "dd-MM-yyyy hh:mm:ss"
+Write-Output "`n= = = = = = = = = = = = = = = = = = = = ="
+Write-Output "Script end time : $ScriptEndTime"
+Write-Output "= = = = = = = = = = = = = = = = = = = = =`n"
+
