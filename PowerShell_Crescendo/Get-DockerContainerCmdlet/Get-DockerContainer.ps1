3c93f1f12cdc4f6573214ac1bdcf846628a45fac
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2024-05-07 15:01:15 +0530

    Add files via upload

---
+++ PowerShell_Crescendo/Get-DockerContainerCmdlet/Get-DockerContainer.ps1
@@ -0,0 +1,114 @@
+# This is a set of commands that accompanies a blog post and Youtube video about PowerShell Crecsendo
+# https://www.youtube.com/mattallford
+# www.mattallford.com
+
+# Install Crescendo
+Install-Module -Name Microsoft.PowerShell.Crescendo -Force
+# Install using PowerShellGet v3
+Install-PSResource Microsoft.PowerShell.Crescendo -Reinstall
+
+# Here are the native docker commands
+docker container list
+docker container list --all
+
+# Create a new PowerShell Crescendo configuration
+$NewConfiguration = @{
+    '$schema' = 'https://aka.ms/PowerShell/Crescendo/Schemas/2021-11'
+    Commands = @()
+}
+$parameters = @{
+    Verb = 'Get'
+    Noun = 'DockerContainer'
+    OriginalName = "C:\Program Files\Docker\Docker\resources\bin\docker.exe"
+}
+$NewConfiguration.Commands += New-CrescendoCommand @parameters
+
+$NewConfiguration | ConvertTo-Json -Depth 3 | Out-File .\docker.crescendo.json
+
+
+# Use crescendo to create a PowerShell module
+Export-CrescendoModule -ConfigurationFile .\docker.crescendo.json -ModuleName docker.crescendo.psm1
+
+# Import the module we just created
+Import-Module .\docker.crescendo.psd1
+
+# View commands available in the module
+Get-Command -module docker.crescendo
+
+# Run the new cmdlet, notice that all it is doing currently is running docker.exe, with no arguments
+Get-DockerContainer
+
+# Go back to the JSON file and add in some arguments by adding OriginalCommandElements
+
+# clean up the powershell module before we make changes and re-import the module
+Remove-Module docker.crescendo
+Remove-Item .\docker.crescendo.psd1
+Remove-Item .\docker.crescendo.psm1
+
+# Use crescendo to create the PowerShell module again
+Export-CrescendoModule -ConfigurationFile .\docker.crescendo.json -ModuleName docker.crescendo.psm1
+
+# Import the module we just created
+Import-Module .\docker.crescendo.psd1
+
+# Run the new cmdlet, this time is should run docker container list, and show that output
+Get-DockerContainer
+
+# clean up the powershell module before we make changes and re-import the module
+Remove-Module docker.crescendo
+Remove-Item .\docker.crescendo.psd1
+Remove-Item .\docker.crescendo.psm1
+
+# Go back to the JSON file and add arguments so the native command exports a string of JSON as a response
+
+# Use crescendo to create a PowerShell module
+Export-CrescendoModule -ConfigurationFile .\docker.crescendo.json -ModuleName docker.crescendo.psm1
+
+# Import the module
+Import-Module .\docker.crescendo.psd1
+
+# Run Get-DockerContainer again, observe that this time we are getting a string of JSON as
+# a response, and we could now pipe that to ConvertFrom-Json to get a native PowerShell object
+Get-DockerContainer
+Get-DockerContainer | ConvertFrom-Json
+
+# Go back to the JSON file and add an output handler
+
+# clean up
+Remove-Module docker.crescendo
+Remove-Item .\docker.crescendo.psd1
+Remove-Item .\docker.crescendo.psm1
+
+# Use crescendo to create a PowerShell module
+Export-CrescendoModule -ConfigurationFile .\docker.crescendo.json -ModuleName docker.crescendo.psm1
+
+# Import the module
+Import-Module .\docker.crescendo.psd1
+
+# Run Get-DockerContainer again, this time we will get a native JSON object and won't
+# need to perform any conversion ourselves
+Get-DockerContainer
+
+# Now let's add in a parameter
+
+# clean up
+Remove-Module docker.crescendo
+Remove-Item .\docker.crescendo.psd1
+Remove-Item .\docker.crescendo.psm1
+
+# Go back to the JSON file and add a parameter named 'all', to call the underlying '--all' argument
+# Use crescendo to create a PowerShell module
+Export-CrescendoModule -ConfigurationFile .\docker.crescendo.json -ModuleName docker.crescendo.psm1
+
+# Import the module
+Import-Module .\docker.crescendo.psd1
+
+# Make Sure Get-DockerContainer with no parameters still does what we want
+Get-DockerContainer
+
+# Call Get-DockerContainer with the -all parameter, which will show all running and not running containers
+Get-DockerContainer -all
+Get-DockerContainer -all | Select-Object names
+
+# See the help for crescendo
+get-help about_crescendo
---
+++ PowerShell_Crescendo/Get-DockerContainerCmdlet/docker.crescendo.psm1
@@ -0,0 +1,268 @@
+# Module created by Microsoft.PowerShell.Crescendo
+# Version: 1.1.0
+# Schema: https://aka.ms/PowerShell/Crescendo/Schemas/2021-11
+# Generated at: 05/07/2024 14:34:27
+class PowerShellCustomFunctionAttribute : System.Attribute {
+    [bool]$RequiresElevation
+    [string]$Source
+    PowerShellCustomFunctionAttribute() { $this.RequiresElevation = $false; $this.Source = "Microsoft.PowerShell.Crescendo" }
+    PowerShellCustomFunctionAttribute([bool]$rElevation) {
+        $this.RequiresElevation = $rElevation
+        $this.Source = "Microsoft.PowerShell.Crescendo"
+    }
+}
+
+# Returns available errors
+# Assumes that we are being called from within a script cmdlet when EmitAsError is used.
+function Pop-CrescendoNativeError {
+param ([switch]$EmitAsError)
+    while ($__CrescendoNativeErrorQueue.Count -gt 0) {
+        if ($EmitAsError) {
+            $msg = $__CrescendoNativeErrorQueue.Dequeue()
+            $er = [System.Management.Automation.ErrorRecord]::new([system.invalidoperationexception]::new($msg), $PSCmdlet.Name, "InvalidOperation", $msg)
+            $PSCmdlet.WriteError($er)
+        }
+        else {
+            $__CrescendoNativeErrorQueue.Dequeue()
+        }
+    }
+}
+# this is purposefully a filter rather than a function for streaming errors
+filter Push-CrescendoNativeError {
+    if ($_ -is [System.Management.Automation.ErrorRecord]) {
+        $__CrescendoNativeErrorQueue.Enqueue($_)
+    }
+    else {
+        $_
+    }
+}
+
+function Get-DockerContainer
+{
+[PowerShellCustomFunctionAttribute(RequiresElevation=$False)]
+[CmdletBinding()]
+
+param(
+[Parameter()]
+[switch]$all
+    )
+
+BEGIN {
+    $PSNativeCommandUseErrorActionPreference = $false
+    $__CrescendoNativeErrorQueue = [System.Collections.Queue]::new()
+    $__PARAMETERMAP = @{
+         all = @{
+               OriginalName = '--all'
+               OriginalPosition = '0'
+               Position = '2147483647'
+               ParameterType = 'switch'
+               ApplyToExecutable = $False
+               NoGap = $False
+               ArgumentTransform = '$args'
+               ArgumentTransformType = 'inline'
+               }
+    }
+
+    $__outputHandlers = @{
+        Default = @{ StreamOutput = $False; Handler = { $args[0] | ConvertFrom-Json } }
+    }
+}
+
+PROCESS {
+    $__boundParameters = $PSBoundParameters
+    $__defaultValueParameters = $PSCmdlet.MyInvocation.MyCommand.Parameters.Values.Where({$_.Attributes.Where({$_.TypeId.Name -eq "PSDefaultValueAttribute"})}).Name
+    $__defaultValueParameters.Where({ !$__boundParameters["$_"] }).ForEach({$__boundParameters["$_"] = get-variable -value $_})
+    $__commandArgs = @()
+    $MyInvocation.MyCommand.Parameters.Values.Where({$_.SwitchParameter -and $_.Name -notmatch "Debug|Whatif|Confirm|Verbose" -and ! $__boundParameters[$_.Name]}).ForEach({$__boundParameters[$_.Name] = [switch]::new($false)})
+    if ($__boundParameters["Debug"]){wait-debugger}
+    $__commandArgs += 'container'
+    $__commandArgs += 'list'
+    $__commandArgs += '--format'
+    $__commandArgs += '{{json .}}'
+    foreach ($paramName in $__boundParameters.Keys|
+            Where-Object {!$__PARAMETERMAP[$_].ApplyToExecutable}|
+            Where-Object {!$__PARAMETERMAP[$_].ExcludeAsArgument}|
+            Sort-Object {$__PARAMETERMAP[$_].OriginalPosition}) {
+        $value = $__boundParameters[$paramName]
+        $param = $__PARAMETERMAP[$paramName]
+        if ($param) {
+            if ($value -is [switch]) {
+                 if ($value.IsPresent) {
+                     if ($param.OriginalName) { $__commandArgs += $param.OriginalName }
+                 }
+                 elseif ($param.DefaultMissingValue) { $__commandArgs += $param.DefaultMissingValue }
+            }
+            elseif ( $param.NoGap ) {
+                # if a transform is specified, use it and the construction of the values is up to the transform
+                if($param.ArgumentTransform -ne '$args') {
+                    $transform = $param.ArgumentTransform
+                    if($param.ArgumentTransformType -eq 'inline') {
+                        $transform = [scriptblock]::Create($param.ArgumentTransform)
+                    }
+                    $__commandArgs += & $transform $value
+                }
+                else {
+                    $pFmt = "{0}{1}"
+                    # quote the strings if they have spaces
+                    if($value -match "\s") { $pFmt = "{0}""{1}""" }
+                    $__commandArgs += $pFmt -f $param.OriginalName, $value
+                }
+            }
+            else {
+                if($param.OriginalName) { $__commandArgs += $param.OriginalName }
+                if($param.ArgumentTransformType -eq 'inline') {
+                   $transform = [scriptblock]::Create($param.ArgumentTransform)
+                }
+                else {
+                   $transform = $param.ArgumentTransform
+                }
+                $__commandArgs += & $transform $value
+            }
+        }
+    }
+    $__commandArgs = $__commandArgs | Where-Object {$_ -ne $null}
+    if ($__boundParameters["Debug"]){wait-debugger}
+    if ( $__boundParameters["Verbose"]) {
+         Write-Verbose -Verbose -Message "C:\Program Files\Docker\Docker\resources\bin\docker.exe"
+         $__commandArgs | Write-Verbose -Verbose
+    }
+    $__handlerInfo = $__outputHandlers[$PSCmdlet.ParameterSetName]
+    if (! $__handlerInfo ) {
+        $__handlerInfo = $__outputHandlers["Default"] # Guaranteed to be present
+    }
+    $__handler = $__handlerInfo.Handler
+    if ( $PSCmdlet.ShouldProcess("C:\Program Files\Docker\Docker\resources\bin\docker.exe $__commandArgs")) {
+    # check for the application and throw if it cannot be found
+        if ( -not (Get-Command -ErrorAction Ignore "C:\Program Files\Docker\Docker\resources\bin\docker.exe")) {
+          throw "Cannot find executable 'C:\Program Files\Docker\Docker\resources\bin\docker.exe'"
+        }
+        if ( $__handlerInfo.StreamOutput ) {
+            if ( $null -eq $__handler ) {
+                & "C:\Program Files\Docker\Docker\resources\bin\docker.exe" $__commandArgs
+            }
+            else {
+                & "C:\Program Files\Docker\Docker\resources\bin\docker.exe" $__commandArgs 2>&1| Push-CrescendoNativeError | & $__handler
+            }
+        }
+        else {
+            $result = & "C:\Program Files\Docker\Docker\resources\bin\docker.exe" $__commandArgs 2>&1| Push-CrescendoNativeError
+            & $__handler $result
+        }
+    }
+    # be sure to let the user know if there are any errors
+    Pop-CrescendoNativeError -EmitAsError
+  } # end PROCESS
+
+<#
+.SYNOPSIS
+unknown shorthand flag: '?' in -?
+See 'docker --help'.
+System.Management.Automation.RemoteException
+Usage:  docker [OPTIONS] COMMAND
+System.Management.Automation.RemoteException
+A self-sufficient runtime for containers
+System.Management.Automation.RemoteException
+Common Commands:
+  run         Create and run a new container from an image
+  exec        Execute a command in a running container
+  ps          List containers
+  build       Build an image from a Dockerfile
+  pull        Download an image from a registry
+  push        Upload an image to a registry
+  images      List images
+  login       Log in to a registry
+  logout      Log out from a registry
+  search      Search Docker Hub for images
+  version     Show the Docker version information
+  info        Display system-wide information
+System.Management.Automation.RemoteException
+Management Commands:
+  builder     Manage builds
+  buildx*     Docker Buildx
+  compose*    Docker Compose
+  container   Manage containers
+  context     Manage contexts
+  debug*      Get a shell into any image or container.
+  dev*        Docker Dev Environments
+  extension*  Manages Docker extensions
+  feedback*   Provide feedback, right in your terminal!
+  image       Manage images
+  init*       Creates Docker-related starter files for your project
+  manifest    Manage Docker image manifests and manifest lists
+  network     Manage networks
+  plugin      Manage plugins
+  sbom*       View the packaged-based Software Bill Of Materials (SBOM) for an image
+  scout*      Docker Scout
+  system      Manage Docker
+  trust       Manage trust on Docker images
+  volume      Manage volumes
+System.Management.Automation.RemoteException
+Swarm Commands:
+  swarm       Manage Swarm
+System.Management.Automation.RemoteException
+Commands:
+  attach      Attach local standard input, output, and error streams to a running container
+  commit      Create a new image from a container's changes
+  cp          Copy files/folders between a container and the local filesystem
+  create      Create a new container
+  diff        Inspect changes to files or directories on a container's filesystem
+  events      Get real time events from the server
+  export      Export a container's filesystem as a tar archive
+  history     Show the history of an image
+  import      Import the contents from a tarball to create a filesystem image
+  inspect     Return low-level information on Docker objects
+  kill        Kill one or more running containers
+  load        Load an image from a tar archive or STDIN
+  logs        Fetch the logs of a container
+  pause       Pause all processes within one or more containers
+  port        List port mappings or a specific mapping for the container
+  rename      Rename a container
+  restart     Restart one or more containers
+  rm          Remove one or more containers
+  rmi         Remove one or more images
+  save        Save one or more images to a tar archive (streamed to STDOUT by default)
+  start       Start one or more stopped containers
+  stats       Display a live stream of container(s) resource usage statistics
+  stop        Stop one or more running containers
+  tag         Create a tag TARGET_IMAGE that refers to SOURCE_IMAGE
+  top         Display the running processes of a container
+  unpause     Unpause all processes within one or more containers
+  update      Update configuration of one or more containers
+  wait        Block until one or more containers stop, then print their exit codes
+System.Management.Automation.RemoteException
+Global Options:
+      --config string      Location of client config files (default
+                           "C:\\Users\\L1085665\\.docker")
+  -c, --context string     Name of the context to use to connect to the
+                           daemon (overrides DOCKER_HOST env var and
+                           default context set with "docker context use")
+  -D, --debug              Enable debug mode
+  -H, --host list          Daemon socket to connect to
+  -l, --log-level string   Set the logging level ("debug", "info",
+                           "warn", "error", "fatal") (default "info")
+      --tls                Use TLS; implied by --tlsverify
+      --tlscacert string   Trust certs signed only by this CA (default
+                           "C:\\Users\\L1085665\\.docker\\ca.pem")
+      --tlscert string     Path to TLS certificate file (default
+                           "C:\\Users\\L1085665\\.docker\\cert.pem")
+      --tlskey string      Path to TLS key file (default
+                           "C:\\Users\\L1085665\\.docker\\key.pem")
+      --tlsverify          Use TLS and verify the remote
+  -v, --version            Print version information and quit
+System.Management.Automation.RemoteException
+Run 'docker COMMAND --help' for more information on a command.
+System.Management.Automation.RemoteException
+For more help on how to use Docker, head to https://docs.docker.com/go/guides/
+System.Management.Automation.RemoteException
+
+.DESCRIPTION See help for C:\Program Files\Docker\Docker\resources\bin\docker.exe
+
+.PARAMETER all
+List all the containers, including those that aren't running.
+
+
+
+#>
+}
+
+
