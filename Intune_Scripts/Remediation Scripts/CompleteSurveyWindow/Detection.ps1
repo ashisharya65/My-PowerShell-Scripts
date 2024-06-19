42b8c74357d1d4a9413c647dd854363537fd30cf
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2024-06-19 23:21:58 +0530

    Create Detection.ps1

---
+++ Intune_Scripts/Remediation Scripts/CompleteSurveyWindow/Detection.ps1
@@ -0,0 +1,13 @@
+
+# Check if the registry key exists and its value
+$KeyPath = "HKLM:\SOFTWARE\CustomReg\FillSurvey"
+$KeyName = "IsSurveyFilled"
+$RegKeyValue = Get-ItemProperty -Path $KeyPath -Name $KeyName -ErrorAction SilentlyContinue
+
+if ($null -eq $RegKeyValue -or $RegKeyValue.$KeyName -ne $true) {
+    Write-Output "Survey not filled"
+    exit 1
+} else {
+    Write-Output "Survey filled"
+    exit 0
+}
