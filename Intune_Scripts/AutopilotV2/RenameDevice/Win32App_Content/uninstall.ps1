8ce7e86dd794ae4b10e59c5a8334efc5ba490416
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2024-08-31 23:21:30 +0530

    Update uninstall.ps1

--- Intune_Scripts/AutopilotV2/RenameDevice/Win32App_Content/uninstall.ps1
+++ Intune_Scripts/AutopilotV2/RenameDevice/Win32App_Content/uninstall.ps1
@@ -1,40 +1,51 @@
+<#
+.SYNOPSIS
+This script removes specific log files, scripts, and scheduled task folders, and then disables and unregisters scheduled tasks related to post-ESP (Enrollment Status Page) reboot operations.
 
-[cmdletbinding()]
-# variable declarations
-param(
-    [string] $PostESPRebootFolderPath = "C:\Temp\Post-ESPReboot",
-    [string] $LogFilePath = $PostESPRebootFolderPath + "\Logs\Post-ESPReboot-UninstallScriptLog.log",
-    [string] $PostESPRebootNotificationScriptPath = $PostESPRebootFolderPath + "\Post-ESPReboot-Notification.ps1",
-    [string] $PostESPRebootScriptPath = $PostESPRebootFolderPath + "\Post-ESPReboot.ps1",
-    [string] $PostESPRebootSchdTask = "Post-ESPReboot",
-    [string] $PostESPRebootNotificationSchdTask = "Post-ESPReboot-Notification"
-)
+.DESCRIPTION
+The script performs the following actions:
+1. Initializes paths for logs, scripts, and scheduled task folders.
+2. Removes the log file, scripts folder, and scheduled tasks folder if they exist.
+3. Disables and unregisters the scheduled tasks "Post-ESPReboot" and "Post-ESPReboot-Notification".
+4. Logs and outputs each step of the script execution for troubleshooting and auditing purposes.
 
-# Function for logging
-Function Write-Log {
-    param(
-        [parameter(mandatory)]
-        [ValidateSet('Info','Error')]
-        [String] $Level,
-        
-        [Parameter(mandatory)]
-        [string] $Message
-    )
-    $CurrentDate = Get-Date -format "dd-MM-yyyy hh:mm:ss"
-    $logmessage = "$($CurrentDate) [$Level] - $Message"
-    Add-Content -path $LogFilePath -Value $logmessage -force
-}
+.PARAMETER RebootSchdTaskName
+The name of the scheduled task responsible for rebooting the system after ESP.
 
-# Removing the Post-ESPReboot folder and its content
-Write-Log -Level 'Info' -Message "Script execution started."
-Remove-Item -path $PostESPRebootNotificationScriptPath -force
-Write-Log -Level 'Info' -Message "Removed the Post-ESPReboot-Notification script."
+.PARAMETER RebootNotificationSchdTaskName
+The name of the scheduled task responsible for notifying the user before the reboot after ESP.
 
-# Disable and unregistering both Post-ESPReboot and Post-ESPReboot-Notification scheduled tasks
-Disable-ScheduledTask -TaskName $PostESPRebootSchdTask -Erroraction 'silentlycontinue' | Out-Null
-Disable-Scheduledtask -TaskName $PostESPRebootNotificationSchdTask -ErrorAction 'Continue' | Out-Null
-Unregister-ScheduledTask -TaskName $PostESPRebootSchdTask -confirm:$false
-Unregister-ScheduledTask -TaskName $PostESPRebootNotificationSchdTask -confirm:$false
-Write-Log -Level 'Info' -Message "Successfully disabled and unregistered both $PostESPRebootSchdTask and $PostESPRebootNotificationSchdTask scheduled tasks."
+.NOTES
+Author: Ashish Arya
+Date: 31-Aug-2024
+Version: 1.0
 
-Write-Log -Level 'Info' -Message "Script execution ended."
+.EXAMPLE
+PS C:\> .\Uninstall.ps1
+This command executes the script, cleaning up the log, scripts, and scheduled tasks related to post-ESP reboots.
+
+#>
+
+# Variable declarations: Define paths for the log folder and scheduled tasks.
+$LogFolderPath = "C:\Temp\Rename-Device"
+$RebootSchdTaskName = "Post-ESPReboot"
+$RebootNotificationSchdTaskName = "Post-ESPReboot-Notification"
+
+# Output the start of the script execution.
+Write-Output "Script execution starts here."
+
+# Remove the log file, scripts folder, and scheduled tasks folder if they exist, excluding the main log file.
+Get-ChildItem -Path $LogFolderPath -Exclude "Rename-Device.log" | Remove-Item -Recurse -Force
+Write-Output "Successfully removed the Log file, Scripts and Scheduled tasks folders."
+
+# Disable the scheduled tasks "Post-ESPReboot" and "Post-ESPReboot-Notification" silently.
+Disable-ScheduledTask -TaskName $RebootSchdTaskName -Erroraction 'silentlycontinue' | Out-Null
+Disable-Scheduledtask -TaskName $RebootNotificationSchdTaskName -ErrorAction 'Continue' | Out-Null
+
+# Unregister the scheduled tasks "Post-ESPReboot" and "Post-ESPReboot-Notification" without confirmation prompts.
+Unregister-ScheduledTask -TaskName $RebootSchdTaskName -confirm:$false
+Unregister-ScheduledTask -TaskName $RebootNotificationSchdTaskName -confirm:$false
+Write-Host "Successfully disabled and unregistered both $RebootSchdTaskName and $RebootNotificationSchdTaskName scheduled tasks."
+
+# Output the end of the script execution.
+Write-Output "Script execution ends here."
